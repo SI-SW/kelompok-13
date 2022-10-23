9aadd1bc7825816631acc92fc0a28300df9a86a9
Risya Aulia <risyaaulia17@gmail.com>
2022-10-23 12:36:59 +0700

    first

---
+++ src/views/Dashboard/ToDo.vue
@@ -0,0 +1,31 @@
+<script>
+import { mapActions, mapState } from 'pinia';
+import d$todo from '@/stores/dashboard/todo';
+
+export default{
+    nama: 'ToDo',
+    computed: {
+        ...mapActions(d$todo, ['g$list']),
+    },
+    methods: {
+        ...mapActions(d$todo, ['a$list']),
+        async getList() {
+            try {
+                await this.a$list();
+            } catch (e) {
+                console.error('method getList error', e);
+            }
+        }
+    },
+    async created(){
+        await this.getList();
+    },
+};
+</script>
+<template>
+<div>
+    <ol>
+        <li v-for="(item, index) in g$list" :key="index">{{item}}</li>
+    </ol>
+</div>
+</template>
